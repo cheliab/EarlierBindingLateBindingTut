28daf20499907c99fd4ba414b168c38fa8efdc1e
Berezkin Pavel <Berezkin.P@dns-shop.ru>
2021-05-29 21:48:20 +1000

    Пример с параметром базового типа в методе

---
+++ PolymorphismTut/Tut_04_MethodWithBasicTypeParameter/Base.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Tut_04_MethodWithBasicTypeParameter
+{
+    /// <summary>
+    /// Базовый класс
+    /// </summary>
+    public class Base
+    {
+        /// <summary>
+        /// Виртуальный метод Info
+        /// </summary>
+        public virtual void Info()
+        {
+            Console.WriteLine("Base.Info()");
+        }
+    }
+}
---
+++ PolymorphismTut/Tut_04_MethodWithBasicTypeParameter/Derived.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Tut_04_MethodWithBasicTypeParameter
+{
+    /// <summary>
+    /// Дочерный класс
+    /// </summary>
+    public class Derived : Base
+    {
+        /// <summary>
+        /// Переопределяем унаследованный метод Info()
+        /// </summary>
+        public override void Info()
+        {
+            Console.WriteLine("Derived.Info()");
+        }
+    }
+}
